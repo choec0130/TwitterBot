fe012f4e16b6421a5fd34f16b727e8db9e06a13f
choec0130 <chloechoe94@gmail.com>
2018-09-19 02:56:30 -0700

    First commit

---
+++ MyBot.js
@@ -0,0 +1,51 @@
+console.log("hello");
+
+var Twit = require('twit');
+var config = require('./config');
+
+var T = new Twit(config);
+
+var params = {
+	q: 'hi',
+	count: 10
+}
+
+T.get('search/tweets', params, gotData);
+
+function gotData(err, data, response) {
+	var tweets = data.statuses;
+	for (var i = 0; i < tweets.length; i++) {
+		console.log(tweets[i].text);
+	}
+}
+function postTweet(myText) {
+	var tweet = {
+		status: myText
+	}
+	T.post('statuses/update', tweet, tweeted);
+
+	function tweeted(err, data, response) {
+		if (err) {
+			console.log("Error in tweeted().");
+			console.log();
+		} else {
+			console.log("It worked.");
+		}
+	}
+}
+
+setInterval(postTweet, 1000*60) { //every minute
+
+}
+
+//set up a user stream
+var stream = T.stream('user');
+
+//anytime someone follows me...
+stream.on('follow', followed);
+
+function followed(eventMsg) {
+	var name = event.source.name;
+	var screenName = eventMsg.source.screen_name;
+	postTweet('@' + screenName + 'Thanks for following!');
+}
