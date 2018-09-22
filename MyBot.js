fac050c924165b80e85f28c9a2ed37e541a1065b
Chloe Choe <chloechoe94@gmail.com>
2018-09-21 23:25:48 -0700

    Update MyBot.js
    Need to add timer to not spam Twitter with retweets

--- MyBot.js
+++ MyBot.js
@@ -10,21 +10,113 @@ var config = require('./config');
 var T = new Twit(config);
 
 var params = {
-	q: 'hi',
+	q: '#giveaway',
 	count: 10
 }
 
-T.get('search/tweets', params, gotData);
+var tweetIdQueue = [];
+
+var id, id_str;
+
+//track public streamed tweets for term 'giveaway'
+var giveawayStream = T.stream('statuses/filter', { track: 'giveaway' })
+
+//anytime a tweet with #giveaway shows up, call gotData
+giveawayStream.on('tweet', function(tweet) {
+	//var for entire JSON of tweets from stream
+	id_str = Object.values(tweet)[2]; //this is id_str
+	
+	tweetIdQueue.push(id_str);
+
+	retweetTweet();
+})
 
 function gotData(err, data, response) {
-	var tweets = data.statuses;
-	for (var i = 0; i < tweets.length; i++) {
-		console.log(tweets[i].text);
+
+	 while (data !== "undefined") {
+	 	//var for entire JSON of tweets from stream
+		var tweets = data.statuses;
+		//works but gets undefined stream--add error catching
+		for (var i = 0; i < tweets.length; i++) {
+			var values = Object.values(tweets[i]);
+			//go for status that doesn't have RT in the beginning
+			if (tweets[i].text.charAt(0) == 'R' && tweets[i].text.charAt(1) == 'T') {
+				//remove from queue
+			} else {
+				console.log(tweets[i].text);
+				console.log(values[2]); //[2] is the id_str
+			}
+		}
+		if (data !== "undefined") {
+			console.log("Waiting for new status to get posted...");
+			setTimeout(gotData, 1000*60*5); //try gotData status again in five minutes
+		}
+	 }
+}
+
+function retweetTweet() {
+
+	if(tweetIdQueue.length > 0) {
+		//  get the id of the firstTweet in the queue
+		var firstTweetId = tweetIdQueue.shift();	
+
+		console.log(firstTweetId);
+
+		T.post('statuses/retweet/:id', { id: firstTweetId }, function (err, data, response) {
+			console.log(data);
+		});
+
+		setTimeout(retweetTweet, 1000*60); //try to retweet again in 1 min
+	} else {
+		setTimeout(retweetTweet, 1000*60); //try to retweet again in 1 minute
 	}
 }
+
+
+
+
+//Node.js modeule to read/write files from local hard drive
+
+function writeToFile(eventMsg) {
+	var fs = require('fs');
+	var json = JSON.stringify(eventMsg,null,2);
+	var myIdValues = Object.values(json.user);
+	id = myIdValues[0];
+	id_str = myIdValues[1];
+	fs.writeFile("tweet.json", json);
+}
+
+
+
+function followed(eventMsg) {
+	var name = event.source.name;
+	var screenName = eventMsg.source.screen_name;
+	postTweet('@' + screenName + 'Thanks for following!');
+}
+
+
+//T.get('search/tweets', params, gotData);
+
+
+
+function tweetEvent(eventMsg) {
+	var tweetReply = eventMsg.in_reply_to_screen_name;
+	var tweetIsFrom = eventMsg.user.screen_name;
+	//make use of what is in tweetText and json file to change
+	//what is tweeted
+	var tweetText = eventMsg.txt;
+
+	if (tweetReply === 'chloeschoe') {
+		var myTweet = '@' + tweetIsFrom + ' hello!'
+		postTweet(myTweet);
+	}
+}
+
+
+//post a tweet with myText
 function postTweet(myText) {
 	var tweet = {
-		status: myText
+		status: "Hello, bot running."
 	}
 	T.post('statuses/update', tweet, tweeted);
 
@@ -33,23 +125,8 @@ function postTweet(myText) {
 			console.log("Error in tweeted().");
 			console.log();
 		} else {
-			console.log("It worked.");
+			console.log("Tweet has posted.");
 		}
 	}
 }
 
-setInterval(postTweet, 1000*60) { //every minute
-
-}
-
-//set up a user stream
-var stream = T.stream('user');
-
-//anytime someone follows me...
-stream.on('follow', followed);
-
-function followed(eventMsg) {
-	var name = event.source.name;
-	var screenName = eventMsg.source.screen_name;
-	postTweet('@' + screenName + 'Thanks for following!');
-}
