f79c656da5af27a113ae5e4ee9663b53f8802594
Chloe Choe <chloechoe94@gmail.com>
2019-02-22 12:45:53 -0500

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@
 // Description: Set your Twitter API consumer key, consumer secret, access token, and access token secret here.
 
 module.exports = {
-	consumer_key: '',
-	consumer_secret: '',
-	access_token: '',
-	access_token_secret: ''
+	consumer_key: '8R3OKDM2dgqT8XKbH4R7HfzgQ',
+	consumer_secret: 'q8eNbpxiEEdY7NjjtnmcXjQeMpUXMvp1zQS5PbVOiGC0FNwAjM',
+	access_token: '1138010744-dYqAeQNGgaR4UPg2OXXVoPkr48Nne4qdo3u6HXe',
+	access_token_secret: 'ROIR1AlPalHcnvlGMtO8PY1wetKHrjsVR84UqhzbXh8px'
 };
